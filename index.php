fda401b10e99a1ab9f8d06f20bc5fe9e470d5840
SeriousBomb <95444039+SeriousBomb@users.noreply.github.com>
2024-06-24 18:11:50 +0500

    Update index.php

--- index.php
+++ index.php
@@ -9,53 +9,9 @@
     <script src="js/java.js"></script>
 </head>
 <body>
-<div class="place">
-<a href="gamesPC.html" ><img src="image/games.jpg" alt="Каталог компьютеров для игр" class="img1"></a>
-        <a href="programsPC.html" ><img src="image/progi.jpg" alt="Каталог компьютеров для работы с программами" class="img2"></a>
-
-    <div class="content">
-        <?php
-            include 'connect/content.php';
-        ?>
-    </div>
-
-
-    <div class="fon"></div>
-        <div class="fon2"></div>
-        <div class="rovnost">
-        <div class="header">
-            <div class="logo">
-            <a href=""><!--<h1>PCstore</h1>--><img src="image/logo.png" class="logoL"></a>
-            </div>
-
-            <?php
-            /*session_name("WebPC");
-            session_start();*/
-            echo "<div class='menu'>
-                <a href='polzovatel/index.php' class='adel'><h3>".$_GET["email"]." Войти</h3></a>
-            </div>
-            <div class='menu'>
-                <a href='reg/index.php' class='adel'><h3>Зарегистрироваться</h3></a>
-            </div>
-            <div class='noone'>
-            <div class='menu'>
-                <a href='catalog/index.php' class='adel'><h3>Каталог</h3></a>
-            </div></div>
-            <!--<div class='menu'>
-                <h3>Корзина</h3>
-            </div>-->
-        </div>
-        </div>";
-        ?>
-
-    <div class="footer">
-        <div class="textGL">Ⓒ Все права защишены.</div>
-    </div>
-
-    
-
-</div>
-
+<?php 
+echo "Hello";
+?>
     
 </body>
 </html>
